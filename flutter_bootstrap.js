df3c839ab765bd176f96c4fd06857a1288ecc3ad
Enoch-Quayson <quaysonenoch1122@gmail.com>
2026-04-03 09:59:29 -0800

    Fix audio URL from HuggingFace

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2900615146" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2305379649" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
