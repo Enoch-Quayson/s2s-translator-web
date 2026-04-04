25489c3d0439e17251f2833ed993f774ea28bcf9
Enoch-Quayson <quaysonenoch1122@gmail.com>
2026-04-04 04:52:50 -0800

    Speed up polling to 2 seconds

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3138757054" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3392380764" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
